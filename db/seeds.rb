ba9f9ff4b83d7b68602a1f37b053dc0aeb80e242
William Lêdo <williamledoc@gmail.com>
2021-09-10 13:10:25 -0300

    66 completa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://img2.gratispng.com/20180604/zya/kisspng-bitcoin-com-cryptocurrency-logo-zazzle-kibuba-btc-5b15aa1f157d09.468430171528146463088.jpg"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/768px-ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://lh3.googleusercontent.com/proxy/fKw5cgajBFIqbEYFOa_4BtVNWFGhfisIoOE4zFjwbg_ESj-Q5VwHzUkAT9hgPKE7cI5b3aOsILo6zyntkfxt9OmR"
+)
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,27 @@
+namespace :dev do
+  desc "Configura o ambiente de desenvolvimento"
+  task setup: :environment do
+    if Rails.env.development?
+
+      show_spinner("Apagando DB...") {%x(rails db:drop)}  # OU do %x(rails db:drop) end
+      show_spinner("Criando DB...") { %(rails db:create)}
+      show_spinner("Migrando DB") {%(rails db:migrate)}
+      show_spinner("Populando DB...") {%(rails db:seed)}
+
+    else
+      puts "Você não está em modo de desenvolvimento"
+    end
+  end
+
+  private
+
+  def show_spinner(msg_start, msg_end = "Concluído!")
+
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}")
+    spinner.auto_spin
+    yield
+    spinner.success("#{msg_end}")
+
+  end
+
+end
